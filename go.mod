97b8d05b403a6752acb787f574029737aea77c57
joshcarp <joshuacarpeggiani@gmail.com>
2020-05-13 22:19:32 +1000

    Add first query version

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/joshcarp/db-2020-2
 
 go 1.14
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
+)
--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-"fmt"
-"database/sql"
-_ "github.com/go-sql-driver/mysql"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/jmoiron/sqlx"
+    "log"
 )
 
 func main() {
-    fmt.Println("Go MySQL Tutorial")
+    fmt.Println("----------")
+    defer fmt.Println("----------")
 
     // Open up our database connection.
     // I've set up a database on my local machine using phpmyadmin.
     // The database is called testDb
-    db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3307)/rugby")
+    //db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3307)/rugby")
+    db, err := sqlx.Open("mysql", "root:1234@tcp(127.0.0.1:3307)/rugby")
+
 
     // if there is an error opening the connection, handle it
     check(err)
@@ -20,15 +24,35 @@ func main() {
     // defer the close till after the main function has finished
     // executing
     defer db.Close()
-
-    q, err := db.Query("SELECT * FROM game;")
+    rows, err := db.Queryx("SELECT * FROM team")
     check(err)
-    fmt.Println()
-    colTypes, err := q.ColumnTypes()
-    check(err)
-    for _, e := range colTypes{
-        fmt.Println(e.Name())
+    type Team struct{
+        TeamID int `db:"TeamID"`
+        TeamName string `db:"TeamName"`
+        TeamType string `db:"TeamType"`
+        ClubID int `db:"ClubID"`
     }
+    var team Team
+    for rows.Next() {
+        err := rows.StructScan(&team)
+        if err != nil {
+            log.Fatalln(err)
+        }
+        fmt.Printf("%#v\n", team)
+    }
+
+
+
+}
+
+func ScanAndPrint(columns int)[]interface{}{
+    var ret []interface{}
+    for i:=0; i<columns;i++{
+        var str string
+        ret = append(ret, &str)
+    }
+    return ret
+
 }
 
 func check(err error){
